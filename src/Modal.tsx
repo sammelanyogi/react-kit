5e95753bb25b5a8466a2c687c889dc34e32f4496
Shady <Shady@EHRs-MacBook-Pro.local>
2020-11-23 16:12:20 +0545

    update modal

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,72 +1,70 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, createElement, ReactPropTypes, useCallback, useContext, useEffect, useRef, useState } from 'react';
 
-type ModalType = React.FunctionComponent<any>;
+type ModalContent = React.FunctionComponent<any>;
 
-type ModalsType = {
-  [key: string]: ModalType;
+type Modal = {
+  show: (props: ReactPropTypes) => void;
+  hide: () => void;
 };
 
-type ModalContentType = {
-  showModal: (key: string, Modal: ModalType) => void;
-  hideModal: (key: string) => void;
+type Modals = Array<{
+  Modal: ModalContent;
+  props: ReactPropTypes;
+}>;
+
+type ModalContextType = {
+  createModal: (Modal: ModalContent) => Modal,
 };
 
 type ModalProviderPropsType = {
   children: React.ReactNode;
-  ModalsContainerComponent: React.FC;
+  ModalsContainerComponent?: React.FC;
 };
 
 
 const getUniqueId = () => Math.floor(Math.random() * 1000).toString();
 
-const ModalContext = createContext<ModalContentType>({
-  showModal: () => {},
-  hideModal: () => {},
+const ModalContext = createContext<ModalContextType>({
+  createModal: (Modal: ModalContent) => ({ show: (props: ReactPropTypes) => {}, hide: () => {}}),
 });
 
-export const ModalProvider: React.FC<ModalProviderPropsType> = ({ children, ModalsContainerComponent }) => {
-  const [modals, setModals] = useState<ModalsType>({});
-  const showModal = (key: string, Modal: ModalType) => {
-    setModals(modals => ({ ...modals, [key]: Modal }));
-  };
-  const hideModal = (key: string) => {
-    setModals(modals => {
-      delete modals[key];
-      return modals;
-    });
-  };
+export const ModalProvider: React.FC<ModalProviderPropsType> = ({ children, ModalsContainerComponent = React.Fragment }) => {
+  const [modals, setModals] = useState<Modals>([]);
 
-  const contextValue = { showModal, hideModal };
+  const createModal = useCallback((Modal: ModalContent) => {
+    const key = getUniqueId();
+    return {
+      show: (props: ReactPropTypes) => setModals(modals => {
+        if (modals.length !== 0 && modals[modals.length-1].props.key === key) return modals;
+        return [...modals.filter(m => m.props.key === key), { Modal, props: { ...props, key }}];
+      }),
+      hide: () => setModals(modals => {
+        return modals.filter(m => m.props.key === key);
+      }),
+    };
+  }, []);
+  
+  const contextValue = { createModal }
 
   return <ModalContext.Provider value={contextValue}>
     {children}
     <ModalsContainerComponent>
-      {Object.keys(modals).map(key => {
-        const Modal = modals[key];
-        return <Modal key={key} />
+      {modals.map(({Modal, props}) => {
+        return createElement(Modal, props);
       })}
     </ModalsContainerComponent>
   </ModalContext.Provider>
 };
 
-export const useModal = (Modal: ModalType) => {
-  const [visible, setVisible] = useState<boolean>(false);
-  const key = getUniqueId();
-  const context = useContext(ModalContext);
-  const show = () => setVisible(true);
-  const hide = () => setVisible(false);
+export const useModal = (ModalContent: ModalContent): Modal => {
+  const modal = useRef<Modal>();
+  const controller = useContext(ModalContext);
 
-  useEffect(() => {
-    if(visible) {
-      context.showModal(key, Modal);
-    } else {
-      context.hideModal(key);
-    }
-    return () => context.hideModal(key);
-  }, [visible]);
+  if (!modal.current) {
+    modal.current = controller.createModal(ModalContent);
+  }
 
-  return {
-    show,
-    hide,
-  };
+  useEffect(() => modal.current.hide, []);
+
+  return modal.current;
 };
