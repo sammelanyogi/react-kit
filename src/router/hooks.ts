ad677c5d6a7d530f42aecdce00458d5316644238
Shady <Shady@EHRs-MacBook-Pro.local>
2020-11-26 16:31:01 +0545

    set initial value for useRoute

--- src/router/hooks.ts
+++ src/router/hooks.ts
@@ -3,7 +3,7 @@ import { Router, RouterContext, ConfirmTransition } from './Router';
 import { Route } from './Route';
 
 export function useRoute(router: Router) {
-  const [route, setRoute] = useState<Route>();
+  const [route, setRoute] = useState<Route>(router.get);
   useEffect(() => router.register(setRoute), [router]);
   return route;
 }
