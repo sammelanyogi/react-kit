6d6dcefc30c4637b6a463b6b2039e1ae313de604
ShadyPilgrim <shovethatshitin@gmail.com>
2021-09-27 12:23:03 +0545

    add onRouteChange prop

--- src/router/Portal.ts
+++ src/router/Portal.ts
@@ -1,21 +1,29 @@
-import { createElement } from 'react';
+import { createElement, useEffect } from 'react';
 import { Router, RouterContext } from './Router.js';
 import { useRoute } from './hooks.js';
+import { Route } from './Route.js';
 
 type Props = {
   router: Router,
+  onRouteChange?: (nextRoute: Route) => void,
 };
 
 /**
  * A portal renders the current route for the given router,
- * and provides the router to the underlying components via
+ * and provides the router to the underlying components viar
  * context.
  */
-export function Portal({ router }: Props) {
+export function Portal({ router, onRouteChange }: Props) {
   // List for change on the route
   const route = useRoute(router);
+
+  useEffect(() => {
+    if (onRouteChange && route) onRouteChange(route);
+  }, [route, onRouteChange]);
+
   // Do not render anything until a route is available
   if (!route) return null;
+  
 
   return createElement(RouterContext.Provider, { value: router }, route.createElement());
 }
