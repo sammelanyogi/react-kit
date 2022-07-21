102c21e9d044d5e910ba299daa2ae030e01346b8
psapien <prateekthapa26@gmail.com>
2022-07-21 15:49:11 +0545

    expose Route

--- src/v2-router/index.ts
+++ src/v2-router/index.ts
@@ -2,3 +2,4 @@ export { Portal } from './Portal.js';
 export type { Router } from './Router.js';
 export { useRouter } from './context.js';
 export { withRouter } from './withRouter.js';
+export { Route } from './Route.js';
