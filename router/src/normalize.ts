0822a0f624f99b8c98e3b4c9d8c040bf2ac52cf2
Ranjan Shrestha <ranjan@sharingapples.com>
2023-01-05 12:01:25 +0545

    Handle '.' and '..' during path normalization

--- router/src/normalize.ts
+++ router/src/normalize.ts
@@ -7,9 +7,20 @@
  * @returns 
  */
 export function normalize(base: string, suffix: string) {
-  if (suffix.startsWith('/')) return suffix;
-  const fullPath = `${base}${suffix}`;
+  const fullPath = suffix.startsWith('/') ? suffix : `${base}${suffix}`;
 
-  //TODO: Remove all './' and '../'
-  return fullPath;
+  const parts = fullPath.split('/');
+  const final = [] as string[];
+  for (let i = 0; i < parts.length; i += 1) {
+    const part = parts[i].trim();
+    if (part === '' || part === '.') continue;
+    if (part === '..') {
+      final.pop();
+      continue;
+    }
+    final.push(part);
+  }
+  
+  return `/${final.join('/')}`;
 }
+
