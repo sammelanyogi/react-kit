7d400b8678deb298a57eae253de53155f722ff2e
Ranjan Shrestha <ranjan@sharingapples.com>
2023-02-03 10:08:59 +0545

    Make sure the initialUrl call is performed only during mount

--- router/src/withRouter.tsx
+++ router/src/withRouter.tsx
@@ -1,35 +1,32 @@
 import React, { useEffect, useState, } from 'react';
-// import { UrlParser } from './Url.js';
 import { RouteMap, RouterDriver } from './types.js';
 import { RouteController, RouteContext } from './RouteController.js';
 import { RouterController, RouterContext } from './RouterController.js';
 
 export function withRouter<Props extends {}, T>(driver: RouterDriver, map: RouteMap<T>, defaultPath: string) {
-  const initialUrl = driver.getInitialUrl();
-
   return (App: React.FC<Props>) => {
     return (props: Props) => {
-      const [initial, setInitial] = useState<RouterController<T> | null>(() => {
-        return typeof initialUrl === 'string' 
-          ? new RouterController(new RouteController('/', map, initialUrl, defaultPath))
-          : null
-      });
+      const [initial, setInitial] = useState<RouterController<T> | null>(null);
       
       useEffect(() => {
-        let router = initial;
+        let router: RouterController<T> = null;
 
+        const initialUrl = driver.getInitialUrl();
         if (typeof initialUrl !== 'string') {
           initialUrl.then((url) => {
             router = new RouterController(new RouteController('/', map, url, defaultPath));
             setInitial(router);
           })
+        } else {
+          router = new RouterController(new RouteController('/', map, initialUrl, defaultPath));
+          setInitial(router);
         }
-        
+
         return driver.subscribe((url) => {
           if (router) {
             router.navigate(router.root, url);
           }
-        })
+        });
       }, []);
 
       if (!initial) return null;
