bae7936765c95c0cc14fe708ea1f14902f138e33
Sammelan Yogi <sammelanyogi@gmail.com>
2023-09-21 16:21:23 +0545

    added multiple error getter

--- form/src/hooks.ts
+++ form/src/hooks.ts
@@ -1,6 +1,6 @@
-import React, { useContext, useState, useEffect, useRef, useMemo } from 'react';
-import { FormController, FormContext } from './FormController.js';
-import { GenericState, FormDefinition } from './types.js';
+import { useContext, useEffect, useRef, useState } from 'react';
+import { FormContext, FormController } from './FormController.js';
+import { FormDefinition, GenericState } from './types.js';
 
 export function useInputError(name: string): undefined | null | Error {
   const controller = useContext(FormContext);
@@ -12,12 +12,48 @@ export function useInputError(name: string): undefined | null | Error {
   return error;
 }
 
+export function useMultipleInputErrors(names?: string[]): (undefined | null | Error)[] {
+  const controller = useContext(FormContext);
+  const [errors, setErrors] = useState<(Error | null | undefined)[]>(() =>
+    names ? names.map(name => controller.checkError(name)) : [],
+  );
+
+  useEffect(() => {
+    return controller.listenErrors(
+      Object.fromEntries(
+        names
+          ? names.map(name => [
+              name,
+              (err: Error) =>
+                setErrors(prev => {
+                  const idx = names.indexOf(name);
+                  if (prev[idx] === err || prev[idx]?.message === err?.message) {
+                    return prev;
+                  }
+                  prev[idx] = err;
+                  return [...prev];
+                }),
+            ])
+          : [],
+      ),
+    );
+  }, [names, controller]);
+
+  if (!controller || !names) {
+    return undefined;
+  }
+  return errors;
+}
+
 export function useFormValue<T extends GenericState>(name: keyof T) {
   const controller = useContext(FormContext);
   return useFormValueOf<T>(controller, name);
 }
 
-export function useFormValueOf<T extends GenericState>(controller: FormController<T>, name: keyof T) {
+export function useFormValueOf<T extends GenericState>(
+  controller: FormController<T>,
+  name: keyof T,
+) {
   const [value, setValue] = useState(controller.get(name));
 
   useEffect(() => {
@@ -27,21 +63,27 @@ export function useFormValueOf<T extends GenericState>(controller: FormControlle
   return value;
 }
 
-export function useFormInput<E=string>(name: string, extractValue?: (evt: E) => string): [string, (evt: E) => void] {
+export function useFormInput<E = string>(
+  name: string,
+  extractValue?: (evt: E) => string,
+): [string, (evt: E) => void] {
   const controller = useContext(FormContext);
   const [value, setValue] = useState(controller.getInput(name));
 
-  return [value, (newValue: E) => {
-    const k = extractValue ? extractValue(newValue) : newValue as never as string;
-    controller.setInput(name, k);
-    setValue(k);
-  }];
+  return [
+    value,
+    (newValue: E) => {
+      const k = extractValue ? extractValue(newValue) : (newValue as never as string);
+      controller.setInput(name, k);
+      setValue(k);
+    },
+  ];
 }
 
 export function useFormController<T extends GenericState>(
   def: FormDefinition<T>,
   initialState?: Partial<T>,
-  action?: string
+  action?: string,
 ) {
   const parentForm = useContext(FormContext);
   const ref = useRef<FormController<T>>();
