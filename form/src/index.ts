4dff828873e382a0aa55b1476d1611073cec0543
Sammelan Yogi <sammelanyogi@gmail.com>
2023-09-21 16:21:23 +0545

    added multiple error getter

--- form/src/index.ts
+++ form/src/index.ts
@@ -1,4 +1,4 @@
 export * from './hooks.js';
-export { Form, FormController } from './FormController.js';
+export { Form, FormController, useFormContext } from './FormController.js';
 export * from './parsers';
 export * from './useFormSubmit.js';
